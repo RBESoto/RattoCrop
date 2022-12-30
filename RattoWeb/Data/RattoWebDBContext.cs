0dc0f1335e6307e20861d7e08dab0ab964e3551e
ESoto <esoto@rattobros.com>
2022-12-30 08:13:38 -0800

    End of Section 2

---
+++ RattoWeb/Data/RattoWebDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RattoWeb.Models.Domain;
+
+namespace RattoWeb.Data
+{
+    public class RattoWebDBContext: DbContext
+    {
+        public RattoWebDBContext(DbContextOptions<RattoWebDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+
+        public DbSet<Walk> Walks { get; set; }
+
+
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+
+    }
+}
